63322bf046e2fb74fdd513696699515523bf6372
Tommy Hagberg <tommy.hagberg@vgregion.se>
2024-08-22 12:55:59 +0200

    ny

--- ADB/Form2.Designer.cs
+++ ADB/Form2.Designer.cs
@@ -36,6 +36,7 @@
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -112,11 +113,21 @@
             this.button8.Text = "Sök biobankanalys";
             this.button8.UseVisualStyleBackColor = true;
             // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(316, 122);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(148, 23);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Analysprotokoll";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(894, 330);
+            this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
@@ -142,5 +153,6 @@
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
     }
 }
