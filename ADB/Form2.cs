698ccb114e26a0de0a2754c8d81d88d04357b416
Tommy Hagberg <tommy.hagberg@vgregion.se>
2024-08-29 13:15:03 +0200

    ny

--- ADB/Form2.cs
+++ ADB/Form2.cs
@@ -47,8 +47,8 @@ namespace ADB
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Form5 f5 = new Form5();
-            f5.ShowDialog(); // Shows Form2
+            Form8 f8 = new Form8();
+            f8.ShowDialog(); // Shows Form8
         }
     }
 }
